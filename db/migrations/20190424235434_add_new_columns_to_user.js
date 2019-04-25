aa5634de10451ea519b6eba38a12051274b11780
tw5033 <tw5033@gmail.com>
2019-04-25 00:15:28 +0000

    Add migration to create phone_number and email columns in users table

--- db/migrations/20190424235434_add_new_columns_to_user.js
+++ db/migrations/20190424235434_add_new_columns_to_user.js
@@ -1,8 +1,24 @@
 
 exports.up = function(knex, Promise) {
-  
+  if(knex.schema.hasTable('users')) {
+    return Promise.all([
+      knex.schema.table('users', (table) => {
+        table.string('phone_number');
+        table.string('email').unique();
+      })
+    ])
+  }
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.table('users', (table) => {
+      if(knex.schema.hasColumn('phone_number')) {
+        table.dropColumn('phone_number');
+      }
+      if(knex.schema.hasColumn('email')) {
+        table.dropColumn('email');
+      }
+    })
+  ])
 };
