e6a0225af4d4640792107473f7299238c7ec6aec
tw5033 <tw5033@gmail.com>
2019-04-25 23:58:02 +0000

    Add img_src for seed files

--- db/seeds/03_food.js
+++ db/seeds/03_food.js
@@ -3,8 +3,11 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return Promise.all([
     // Inserts seed entries
-    knex('foods').insert({id: 1, name: 'Cake', description: 'good food', price: 2.50}),
-    knex('foods').insert({id: 2, name: 'Chocolate Cookie', description: 'good food', price: 2.50}),
-    knex('foods').insert({id: 3, name: 'Fruit Tart', description: 'good food', price: 2.50})
+    knex('foods').insert({id: 1, name: 'Clam', description: 'Fresh clams, sold by the pound', price: 10, img_src:"/images/clams.jpg"}),
+    knex('foods').insert({id: 2, name: 'Lobster', description: 'Cleaned lobster', price: 15, img_src:"/images/lobster.jpg"}),
+    knex('foods').insert({id: 3, name: 'Mussel', description: 'Fresh mussels, sold by the pound', price: 4, img_src:"/images/mussels.jpg"}),
+    knex('foods').insert({id: 4, name: 'Oysters', description: 'Fresh oysters, sold by the pound', price: 15, img_src:"/images/oysters.jpg"}),
+    knex('foods').insert({id: 5, name: 'Prawns', description: 'African prawns, in packs of 12', price: 15, img_src:"/images/prawns.jpg"}),
+    knex('foods').insert({id: 6, name: 'Sockeye Salmon', description: 'Caught from the Pacific Northwest', price: 100, img_src:"/images/salmon.jpg"})
   ]);
 };
