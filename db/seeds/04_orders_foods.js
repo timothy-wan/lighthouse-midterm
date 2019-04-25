98512b93b0f6ff888aa8572756ae902da0bcfc26
tw5033 <tw5033@gmail.com>
2019-04-25 04:11:35 +0000

    Add seeds for orders_foods table

--- db/seeds/04_orders_foods.js
+++ db/seeds/04_orders_foods.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('orders_foods').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('orders_foods').insert({id: 1, quantity: 2, ordersid: 1, foodsid: 2}),
+        knex('orders_foods').insert({id: 2, quantity: 1, ordersid: 1, foodsid: 3}),
+        knex('orders_foods').insert({id: 3, quantity: 1, ordersid: 1, foodsid: 1}),
+        knex('orders_foods').insert({id: 4, quantity: 1, ordersid: 2, foodsid: 2}),
+        knex('orders_foods').insert({id: 5, quantity: 1, ordersid: 3, foodsid: 1}),
       ]);
     });
 };
