5a876fc8386379ee4cc1dd623cd8e049160fe5a0
tw5033 <tw5033@gmail.com>
2019-04-25 01:08:07 +0000

    Increase seed data for users table

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,10 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex, Promise) { // eslint-disable-line no-unused-vars
   return knex.schema.createTable('users', function (table) {
     table.increments();
     table.string('name');
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex, Promise) { // eslint-disable-line no-unused-vars
   return knex.schema.dropTable('users');
 };
