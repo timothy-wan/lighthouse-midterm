22853712a853560ce0421b4cb38a70a57c97c00e
Theo Stavrides <theostavrides@gmail.com>
2019-04-26 02:12:23 +0000

    modularize code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,28 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+function writeItemQuantities() {
+  const $counts = $(".count")
+  $.each($counts, function(i, val){
+    const itemID = $(val).siblings().eq(0).attr('data-foodid');
+    if (localStorage.getItem(itemID)) {
+      $(val).html(localStorage.getItem(itemID))
     }
-  });;
-});
+  })
+}
+
+function quantityCounter(event){
+  let buttonType = $(event.target).attr('class');
+  let foodid = $(event.target).attr('data-foodid');
+  if (buttonType === 'increment') {
+    let oldQuantity = Number(localStorage.getItem(foodid));
+    let newQuantity = oldQuantity + 1;
+    localStorage.setItem(foodid, newQuantity);
+    $(event.target).siblings('div').html(newQuantity);
+  }
+  if (buttonType === 'decrement') {
+    let oldQuantity = Number(localStorage.getItem(foodid));
+    let newQuantity = oldQuantity - 1;
+    if (newQuantity >= 0) {
+      localStorage.setItem(foodid, newQuantity);
+      $(event.target).siblings('div').html(newQuantity);
+    }
+  }
+}
