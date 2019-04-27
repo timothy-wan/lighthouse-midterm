0624ec0e22fae4ac09c3da5631a08ef5e4efb935
Theo Stavrides <theostavrides@gmail.com>
2019-04-27 19:51:51 +0000

    add crab animations

--- public/scripts/pending.js
+++ public/scripts/pending.js
@@ -1,6 +1,33 @@
 $(()=>{
 
   //check to see if oder status has changed on the server every 10 seconds
+
+  setInterval(function(){
+    if ($('#status').data('orderstatus') === 'pending') {
+
+      //pending dots
+      let $dots = $('#dots')
+      if ($dots.text() === '') {
+        $dots.text('.');
+      } else if ($dots.text() === '.') {
+        $dots.text('..')
+      } else if ($dots.text() === '..') {
+        $dots.text('...')
+      }else if ($dots.text() === '...') {
+        $dots.text('')
+      }
+
+      //crab animation
+      let $img = $('#animation img');
+      let imgsrc = $img.attr('src');
+      if (imgsrc === '/images/animation/pot1.png') {
+        $img.attr('src', '/images/animation/pot2.png');
+      } else {
+        $img.attr('src', '/images/animation/pot1.png');
+      }
+    }
+  }, 1000)
+
   setInterval(function(){
     if ($('#status').data('orderstatus') === 'pending') {
       $.get('/api/orders', function(orders){
