2ecc6835081d95a64f31e9428a2a466426ff38ad
Theo Stavrides <theostavrides@gmail.com>
2019-04-28 18:22:03 +0000

    fix pending page

--- public/scripts/pending.js
+++ public/scripts/pending.js
@@ -39,13 +39,15 @@ $(()=>{
   }, 1000)
 
   setInterval(function(){
-    if ($('#status').data('orderstatus') === 'pending') {
+    let currentStatus = $('#status').data('orderstatus');
+    if (currentStatus === 'pending') {
       $.get('/api/orders', function(orders){
         let orderid = $('#order').data('orderid');
 
         // TODO this is really stupid, we need a database helper to get 1 order
         let serverOrderStatus = orders.filter(o => o.id === orderid)[0].status;
-        $('#status').data('orderstatus') = serverOrderStatus;
+        $('#status').data('orderstatus', serverOrderStatus);
+        if (serverOrderStatus){}
         console.log('current order status: ', serverOrderStatus);
       })
     }
